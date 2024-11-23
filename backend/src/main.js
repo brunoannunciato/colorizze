bb3333f84dc31a70409abe43c483e309345fc118
brunoannunciato <brunobannunciato@gmail.com>
2024-11-23 00:54:29 -0300

    feat: allow cors

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+
 const pool = require('./db/index')
 
 const app = express()
 const PORT = 3001
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({ status: 'ok'})
